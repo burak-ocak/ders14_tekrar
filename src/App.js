271a8a98e315c47c25429117cc9cbaa902fb92c1
Burak Ocak <ocakbura@gmail.com>
2023-04-28 16:42:52 +0300

    feat: useContext örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
 
 function App() {
   return (
     <div>
-      Ben app componenti
+      <Component1 />
     </div>
   );
 }
+
+function Component1() {
+  const [user, setUser] = useState("Jesse Hall");
+
+  return (
+    <>
+      <h1>{`Hello ${user}!`}</h1>
+      <UserContext.Provider value={user}>
+        <Component2 />
+      </UserContext.Provider>
+    </>
+  );
+}
+
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext)
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
+}
+
 
 export default App;
